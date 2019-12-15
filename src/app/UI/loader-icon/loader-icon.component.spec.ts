c3b53eedbf8731cbdd2b31d18ca4d592b12d97b5
Adrian Zablocki <adrian.zablocki@icloud.com>
2019-12-15 14:37:07 +0100

    test icon loader component

--- src/app/UI/loader-icon/loader-icon.component.spec.ts
+++ src/app/UI/loader-icon/loader-icon.component.spec.ts
@@ -3,23 +3,33 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoaderIconComponent } from './loader-icon.component';
 
 describe('LoaderIconComponent', () => {
-  let component: LoaderIconComponent;
-  let fixture: ComponentFixture<LoaderIconComponent>;
+    let component: LoaderIconComponent;
+    let fixture: ComponentFixture<LoaderIconComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LoaderIconComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [LoaderIconComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LoaderIconComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LoaderIconComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should render spinner wrapper', () => {
+        const fixture = TestBed.createComponent(LoaderIconComponent);
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('.lds-double-ring')).toBeTruthy();
+    });
+
+    it('should render spinner elements', () => {
+        const fixture = TestBed.createComponent(LoaderIconComponent);
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelectorAll('.lds-double-ring div').length === 2).toBeTruthy();
+    });
 });
